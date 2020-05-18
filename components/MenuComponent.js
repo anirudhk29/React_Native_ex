38a7779fca0d5f1e2a130309a4504c9355211a1c
Anirudh Koolwal <koolani81@gmail.com>
2020-05-18 18:00:15 +0530

    Components Part 2

--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -7,12 +7,14 @@ function Menu(props) {
   const renderMenuItem = ({item ,index}) => {
     return (
       <ListItem key={index} title={item.name} subtitle={item.description}
-        hideChevron={true} leftAvatar={{ source: require("./images/uthappizza.png")}} />
+        hideChevron={true} leftAvatar={{ source: require("./images/uthappizza.png")}}
+        onPress={() => props.onPress(item.id)}  />
     );
   };
 
   return (
-    <FlatList data={props.dishes} renderItem={renderMenuItem} keyExtractor={item =>item.id.toString()} />
+    <FlatList data={props.dishes} renderItem={renderMenuItem}
+      keyExtractor={item =>item.id.toString()} />
   );
 }
 
