ae54198cdd3a9ba2bfa0bcf8d78cd796ebcc2055
Anirudh Koolwal <koolani81@gmail.com>
2020-05-29 17:00:16 +0530

    Gestures Part 1

--- components/DishdetailComponent.js
+++ components/DishdetailComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Text, View, ScrollView, FlatList, Modal, Button } from "react-native";
+import { Text, View, ScrollView, FlatList, Modal, Button, Alert, PanResponder } from "react-native";
 import { Card, Icon, Rating, Input } from "react-native-elements";
 import { connect } from "react-redux";
 import { baseUrl } from "../shared/baseUrl";
@@ -20,10 +20,44 @@ const mapDispatchToProps = dispatch => ({
 })
 
 function RenderDish(props) {
+
+  const recognizeDrag = ({ moveX, moveY, dx, dy}) => {
+    if(dx < -200) 
+      return true;
+    else
+      return false;
+
+  }
+
+  const panResponder = PanResponder.create({
+    onStartShouldSetPanResponder : (e, getsureState) => {
+      return true;
+    },
+    onPanResponderEnd : (e, getsureState ) => {
+      console.log("Pan responder end" ,  getsureState);
+      if(recognizeDrag(getsureState)) 
+        Alert.alert("Add Favorite",
+         "Are you sure you wish to add "+dish.name+" to favorite?",
+        [
+          {
+            text:"Cancel", onPress: () => console.log("Cancel Pressed"),
+            style:"cancel"
+          },
+          {
+            text: "OK", onPress: () =>{ props.favorite ? console.log("Already favourite")
+            : props.onPress()}
+          }
+        ],
+        {cancelable: false}   
+        );
+      return true;
+    }
+  })
+
   const dish = props.dish;
   if(dish != null) {
       return(
-        <Animatable.View animation="fadeInDown" duration={2000} delay={1000} >
+        <Animatable.View animation="fadeInDown" duration={2000} delay={1000} {...panResponder.panHandlers} >
           <Card featuredTitle = {dish.name}
             image={{uri : baseUrl + dish.image}}>
             <Text style={{margin : 10}}> {dish.description} </Text>
